c6b41062bab5da7880e3be2c3fe8c8e842ed9663
KanchanaWorkspace <rmmkr2018@gmail.com>
2026-02-23 17:43:14 +0530

    Write file example

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+const fs2 = require('fs');
+fs2.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
